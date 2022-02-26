dc50580e7d1a2df840f1f877529f5ca15d415afd
Xiang Zheng <xzheng235-c@my.cityu.edu.hk>
2022-02-26 13:52:53 -0500

    update

--- tut6/call_sh_32.c
+++ tut6/call_sh_32.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-#include <string.h>
-
 // from http://www.kernel-panic.it/security/shellcode/index.html
 // char *inner_argv[] = {"/bin/sh", NULL};
 // execve(inner_argv[0], inner_argv, NULL);
-char code[] =
+char shellcode[] =
+    // "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
     "\xeb\x18\x5e\x31\xc0\x88\x46\x07\x89\x76\x08\x89\x46\x0c\xb0\x0b"
     "\x8d\x1e\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\xe8\xe3\xff\xff\xff\x2f"
     "\x62\x69\x6e\x2f\x73\x68";
 
 int main()
 {
-    ((void (*)())code)();
+    ((void (*)())shellcode)();
     return 1;
 }
